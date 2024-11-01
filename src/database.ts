64a0766533b24e4e9f8f8f902fbd3099fcf2dbeb
Markus Danilow <markus.danilow@coordyek.com>
2024-11-01 23:24:21 +0100

    possible fix for database

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,13 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Room } from './entity/Room';
 import { SensorData } from './entity/SensorData';
+import path from 'path';
 
 export const AppDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'database.sqlite',
+    type: "sqlite",
+    database: process.env.NODE_ENV === "production"
+        ? ":memory:"
+        : path.join(__dirname, "database.sqlite"),
     synchronize: true,
     entities: [Room, SensorData],
 });
